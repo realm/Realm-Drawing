9abc2d03eca63f7c15404d4236c058e8ca9beca7
Thomas Goyne <thomas.goyne@mongodb.com>
2021-12-16 13:22:21 -0800

    Adopt custom type mappings

--- DrawingApp/Model/Line.swift
+++ DrawingApp/Model/Line.swift
@@ -6,55 +6,28 @@
 //
 
 import Foundation
-import SwiftUI
 import RealmSwift
+import SwiftUI
+
+extension CGFloat: CustomPersistable {
+    public typealias PersistedType = Double
+    public init(persistedValue: Double) {
+        self = persistedValue
+    }
+    public var persistableValue: Double {
+        self
+    }
+}
 
 class Line: EmbeddedObject, ObjectKeyIdentifiable {
-    @Persisted var lineColor: PersistableColor?
-    @Persisted var lineWidth = 5.0
-    @Persisted var linePoints = RealmSwift.List<PersistablePoint>()
-    
+    @Persisted var color: Color
+    @Persisted var width: CGFloat = 5.0
+    @Persisted var points = RealmSwift.List<CGPoint>()
 }
 
 extension Line {
-    var points: [CGPoint] {
-        linePoints.map { point in
-            point.point
-        }
-    }
-    
-    var color: Color {
-        get {
-            lineColor?.color ?? .black
-        }
-        set {
-            lineColor = PersistableColor(color: newValue)
-        }
-    }
-    
-    var width: CGFloat {
-        get {
-            CGFloat(lineWidth)
-        }
-        set {
-            lineWidth = Double(newValue)
-        }
-    }
-    
-    convenience init (point: PersistablePoint, color: Color, lineWidth: CGFloat) {
+    convenience init(color: Color, lineWidth: CGFloat) {
         self.init()
-        self.linePoints.append(point)
-        self.color = color
-        self.width = lineWidth
-    }
-    
-    convenience init (points: [CGPoint], color: Color, lineWidth: CGFloat, xScale: CGFloat, yScale: CGFloat) {
-        self.init()
-        let persistablePoints = RealmSwift.List<PersistablePoint>()
-        points.forEach() { point in
-            persistablePoints.append(PersistablePoint(x: point.x / xScale, y: point.y / yScale))
-        }
-        self.linePoints = persistablePoints
         self.color = color
         self.width = lineWidth
     }
